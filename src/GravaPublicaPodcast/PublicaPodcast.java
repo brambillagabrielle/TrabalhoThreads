4e3267e46cfe163a1af4864b4f2aa6f2e67f39d4
Gabrielle Brambilla <69489731+brambillagabrielle@users.noreply.github.com>
2022-05-26 21:59:12 -0300

    v2

--- src/GravaPublicaPodcast/PublicaPodcast.java
+++ src/GravaPublicaPodcast/PublicaPodcast.java
@@ -1,27 +1,60 @@
 package GravaPublicaPodcast;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
-* Classe PublicaPodcast para representar a thread de publicação de um podcast gravado
+* Classe PublicaPodcast para representar a thread de publicação de um podcast
+* gravado.
 * 
 * @author Gabrielle Brambilla (20201PF.CC0190)
 */
-
 public class PublicaPodcast extends Thread {
     
-    Podcast podcast;
+    private ArrayBlockingQueue<String> getFilaPublicacao;
+    private String assuntoGravado;
     
-    public PublicaPodcast(Podcast podcast) {
-        this.podcast = podcast;
+    public PublicaPodcast(ArrayBlockingQueue<String> getFilaPublicacao) {
+        this.getFilaPublicacao = getFilaPublicacao;
+        this.assuntoGravado = "";
     }
     
+    public ArrayBlockingQueue<String> getFilaPublicacao() {
+        return getFilaPublicacao;
+    }
+
+    public void setAssuntoGravado(String assuntoGravado) {
+        this.assuntoGravado = assuntoGravado;
+    }
+
+    public String getAssuntoGravado() {
+        return assuntoGravado;
+    }
+    
+    public void mostraPublicacao(int episodio) {
+        System.out.println("Publicado: Ep #" + episodio + " - " + getAssuntoGravado());
+    }
+    
+    /**
+    * Método run pertencente à classe Thread, com o código que é executado em
+    * paralelo. Nesse caso o código pega podcasts gravados que foram enviados
+    * para a fila BlockingQueue (fila de publicação) e publica os episódios.
+    */
+    @Override
     public void run() {
+        
+        int episodio = 0;
         
         try {
             
-            for (int i = 1; i <= 10; i++) {
+            while (true) {
                 
-                System.out.println("Podcast publicado: " + this.podcast.getPodcast());
-                Thread.sleep((int) Math.random() * 1000);
+                if (!getFilaPublicacao().isEmpty()) {
+                    
+                    episodio++;
+                    setAssuntoGravado(getFilaPublicacao().take());
+                    mostraPublicacao(episodio);
+                    
+                }
                 
             }
             
